6a3b962e493f2e85c2215c1439d531303f456a76
NoorTushar <tushar.noor.khan@gmail.com>
2024-03-26 02:02:48 +0600

    remove left padding from toggle button

--- src/components/Shared/NavBar.jsx
+++ src/components/Shared/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
                <div
                   tabIndex={0}
                   role="button"
-                  className="btn btn-ghost lg:hidden"
+                  className="btn btn-ghost lg:hidden pl-0"
                >
                   <svg
                      xmlns="http://www.w3.org/2000/svg"
@@ -28,7 +28,7 @@ const NavBar = () => {
                </div>
                <ul
                   tabIndex={0}
-                  className="menu menu-md dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 space-y-2"
+                  className="menu menu-md dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 space-y-2 "
                >
                   <li>
                      <NavLink to={"/"}>Home</NavLink>
