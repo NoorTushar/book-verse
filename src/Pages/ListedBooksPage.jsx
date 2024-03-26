2c8a210a060856038ac918279184bae863911733
NoorTushar <tushar.noor.khan@gmail.com>
2024-03-26 14:31:23 +0600

    handleTabIndex is functional and css also dynamic in both listed and wishlist  tabs

--- src/Pages/ListedBooksPage.jsx
+++ src/Pages/ListedBooksPage.jsx
@@ -1,8 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 import { Link, Outlet } from "react-router-dom";
 
 const ListedBooksPage = () => {
+   const [tabIndex, setTabIndex] = useState(0);
+
+   useEffect(() => {
+      const localIndex = JSON.parse(localStorage.getItem("tabIndex"));
+      if (localIndex !== null) {
+         setTabIndex(localIndex);
+      }
+   }, []);
+
+   const handleTabIndex = (index) => {
+      setTabIndex(index);
+      localStorage.setItem("tabIndex", JSON.stringify(index)); // Store the updated index
+   };
+
    return (
       <div>
          <h1 className="text-2xl lg:text-3xl rounded-lg py-5 font-bold text-center bg-[#F3F3F3]">
@@ -28,22 +42,28 @@ const ListedBooksPage = () => {
                </ul>
             </div>
          </div>
-
          {/* Tabs */}
          <div className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden sm:justify-start pl-5 flex-nowrap dark:text-gray-800">
             <Link
+               onClick={() => handleTabIndex(0)}
                to={`/listedBooks`}
-               className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border-b dark:border-gray-600 dark:text-gray-600"
+               className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 dark:border-gray-600 dark:text-gray-600 ${
+                  tabIndex == 0 ? "border border-b-0 rounded-t-lg" : "border-b"
+               }`}
             >
                <span>Listed Books</span>
             </Link>
             <Link
+               onClick={() => handleTabIndex(1)}
                to={`/listedBooks/wishlist`}
-               className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border border-b-0 rounded-t-lg dark:border-gray-600 dark:text-gray-900"
+               className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 dark:border-gray-600 dark:text-gray-900 ${
+                  tabIndex == 1 ? "border border-b-0 rounded-t-lg" : "border-b"
+               }`}
             >
                <span>Wishlist Books</span>
             </Link>
          </div>
+
          <Outlet></Outlet>
       </div>
    );
