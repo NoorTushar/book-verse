c73fceb1458b4b506289271f4dabf76e15c4e9cf
NoorTushar <tushar.noor.khan@gmail.com>
2024-03-28 15:18:55 +0600

    blog card section done, now will work on individual card.

--- src/Pages/BlogsPage.jsx
+++ src/Pages/BlogsPage.jsx
@@ -1,7 +1,50 @@
+import { useLoaderData } from "react-router-dom";
+import BlogCard from "../components/BlogCard";
+
 const BlogsPage = () => {
+   const blogs = useLoaderData();
+
    return (
       <div>
-         <h3>Blogs Page</h3>
+         <h1 className="text-2xl lg:text-3xl rounded-lg py-5 font-bold text-center bg-[#F3F3F3] mt-6">
+            Blogs
+         </h1>
+
+         {/* Blogs Container */}
+         <section className=" text-gray-100 my-6">
+            <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12 px-0">
+               <div className="block max-w-sm gap-3 mx-auto sm:max-w-full group lg:grid lg:grid-cols-12 text-black">
+                  <img
+                     src="https://source.unsplash.com/random/480x360"
+                     alt=""
+                     className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 bg-gray-500 "
+                  />
+                  <div className="p-6 space-y-4 lg:col-span-5">
+                     <h3 className="text-2xl font-semibold sm:text-4xl ">
+                        {blogs[0].title}
+                     </h3>
+                     <span
+                        className="text-xs text-black
+                     "
+                     >
+                        {blogs[0].date}
+                     </span>
+                     <p>{blogs[0].content.slice(0, 170)} ...</p>
+                     <button className="btn bg-our-primary text-white hover:bg-green-600">
+                        Read More
+                     </button>
+                  </div>
+               </div>
+
+               <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                  {/* each blog card */}
+
+                  {blogs.map((blog) => {
+                     return <BlogCard key={blog.blogId} blog={blog}></BlogCard>;
+                  })}
+               </div>
+            </div>
+         </section>
       </div>
    );
 };
